97705d874c4420eb92b497a34f6a6d6073224b64
SSC CI Test User <ssc@citestuser.com>
2020-12-10 14:19:37 +0000

    Initial Commit

---
+++ src/test-github-actions-cookiecutter-cpp-project.cpp
@@ -0,0 +1,9 @@
+#include "test-github-actions-cookiecutter-cpp-project/test-github-actions-cookiecutter-cpp-project.hpp"
+
+namespace testgithubactionscookiecuttercppproject {
+
+int add_one(int x){
+  return x + 1;
+}
+
+} // namespace testgithubactionscookiecuttercppproject
