d724a7114def0b101a9ad0bce5804a9c2aafb62a
SSC CI Test User <ssc@citestuser.com>
2021-05-17 05:15:34 +0000

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='testgithubactionscookiecuttercppproject',
-    version='0.0.1',
+    version="0.0.59",
     author='Your Name',
     author_email='your@email.com',
     description='Add description here',
